39c1ba382c56b133205e3b618e9a8a6d205f19a8
Razsinxron <106243714+Razsinxron@users.noreply.github.com>
2024-11-07 12:18:27 +0200

    solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,11 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ commentList }) => {
+  return (
+    <div className="CommentList">
+      {commentList.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,13 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ serverProps }) => {
+  // console.log('server', serverProps);
+
+  return (
+    <div className="PostList">
+      {serverProps.map(postProp => (
+        <PostInfo key={postProp.id} postProp={postProp} />
+      ))}
+    </div>
+  );
+};
